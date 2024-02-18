00ec79e48e1a50d1c2caf8b2f2da0f1aa5b63b23
js4000all <js4000all@gmail.com>
2024-02-18 11:00:41 +0000

    comparing the guess

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -1,11 +1,19 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let secret: u64 = rand::thread_rng().gen_range(1..=10);
     println!("Please input your guess.");
-    let mut guess = String::new();
+    let mut buf = String::new();
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut buf)
         .expect("Failed to read line.");
-    
-    println!("Your guessed: {guess}")
+    let guess: u64 = buf.trim().parse().expect("Please type a number");
+    println!("Your guessed: {buf}");
+    match guess.cmp(&secret){
+        Ordering::Less => println!("Too small. actual={secret}"),
+        Ordering::Greater => println!("Too big. actual={secret}"),
+        _ => println!("You win")
+    }
 }
