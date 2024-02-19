87057f3639ccdb8bd56c45e53bb6233ed5b23c09
js4000all <js4000all@gmail.com>
2024-02-19 11:56:56 +0000

    avoid to use "except" method.

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -6,10 +6,21 @@ fn main() {
     let secret: u64 = rand::thread_rng().gen_range(1..=10);
     println!("Please input your guess.");
     let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-    let guess: u64 = guess.trim().parse().expect("Please type a number");
+    let _n = match io::stdin().read_line(&mut guess){
+        Ok(n) => n,
+        Err(_) => {
+            println!("Failed to read line.");
+            return;
+        }
+    };
+
+    let guess: u64 = match guess.trim().parse(){
+        Ok(n) => n,
+        Err(_) => {
+            println!("Failed to parse input.");
+            return;
+        }
+    };
     println!("Your guessed: {guess}");
     match guess.cmp(&secret){
         Ordering::Less => println!("Too small. actual={secret}"),
