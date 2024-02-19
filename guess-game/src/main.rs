55abf948e99d6d4bd6bafb3479d36ef109f3c716
js4000all <js4000all@gmail.com>
2024-02-19 10:16:38 +0000

    I accept shadowing

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -5,12 +5,12 @@ use std::io;
 fn main() {
     let secret: u64 = rand::thread_rng().gen_range(1..=10);
     println!("Please input your guess.");
-    let mut buf = String::new();
+    let mut guess = String::new();
     io::stdin()
-        .read_line(&mut buf)
+        .read_line(&mut guess)
         .expect("Failed to read line.");
-    let guess: u64 = buf.trim().parse().expect("Please type a number");
-    println!("Your guessed: {buf}");
+    let guess: u64 = guess.trim().parse().expect("Please type a number");
+    println!("Your guessed: {guess}");
     match guess.cmp(&secret){
         Ordering::Less => println!("Too small. actual={secret}"),
         Ordering::Greater => println!("Too big. actual={secret}"),
