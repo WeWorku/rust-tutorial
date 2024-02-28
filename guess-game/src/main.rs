5ae8398ea7a955975dc84c3899ae30575ac89d79
js4000all <js4000all@gmail.com>
2024-02-28 12:58:59 +0000

    allow mutiple guesses with looping

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -4,27 +4,33 @@ use std::io;
 
 fn main() {
     let secret: u64 = rand::thread_rng().gen_range(1..=10);
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    let _n = match io::stdin().read_line(&mut guess){
-        Ok(n) => n,
-        Err(_) => {
-            println!("Failed to read line.");
-            return;
-        }
-    };
 
-    let guess: u64 = match guess.trim().parse(){
-        Ok(n) => n,
-        Err(_) => {
-            println!("Failed to parse input.");
-            return;
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        let _n = match io::stdin().read_line(&mut guess) {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Failed to read line.");
+                return;
+            }
+        };
+
+        let guess: u64 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Invalid input. Aborting.");
+                return;
+            }
+        };
+        println!("Your guessed: {guess}");
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small."),
+            Ordering::Greater => println!("Too big."),
+            _ => {
+                println!("You win");
+                break;
+            }
         }
-    };
-    println!("Your guessed: {guess}");
-    match guess.cmp(&secret){
-        Ordering::Less => println!("Too small. actual={secret}"),
-        Ordering::Greater => println!("Too big. actual={secret}"),
-        _ => println!("You win")
     }
 }
